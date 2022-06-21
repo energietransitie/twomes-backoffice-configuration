bc8e143c43e38d7653df29d55262ea544ab1dab8
hterhofte <h.ter.hofte@windesheim.nl>
2022-06-21 09:30:01 +0200

    various naming changes mostly volumes

--- duplicati/script.sh
+++ duplicati/script.sh
@@ -12,9 +12,9 @@ if [ "$DUPLICATI__EVENTNAME" == "BEFORE" ]
 then
 	if [ "$DUPLICATI__OPERATIONNAME" == "Backup" ]
 	then
-		echo "test"
+		echo "BEFORE docker command to do backup"
 		docker exec mariadb_$DUPLICATI__backup_name mysqldump --opt --no-autocommit --user root --password=$DB_PASSWORD twomes > /dump_$DUPLICATI__backup_name/db.dump
-		echo "test2"
+		echo "AFTER docker command to do backup"
 	fi
 
 elif [ "$DUPLICATI__EVENTNAME" == "AFTER" ]
@@ -22,7 +22,9 @@ then
 
         if [ "$DUPLICATI__OPERATIONNAME" == "Restore" ]
         then
+                echo "BEFORE docker command to do Restore"
                 docker exec -i mariadb_$DUPLICATI__backup_name mysql --user root --password=$DB_PASSWORD twomes < /dump_$DUPLICATI__backup_name/db.dump
+		echo "AFTER docker command to do Restore"
         fi
 fi
 
